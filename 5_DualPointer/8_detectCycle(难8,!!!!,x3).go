2b09732a68e52c7a3241f1dadcdaa6a9d5375f93
h31029 <hzy740876073@gmail.com>
2024-12-05 11:51:31 +0800

    【双指针】之8：检测环，找到环入口

--- 5_DualPointer/8_detectCycle(难8,!!!!,x3).go
+++ 5_DualPointer/8_detectCycle(难8,!!!!,x3).go
@@ -12,21 +12,36 @@ import (
 */
 func DetectCycle(head *linknode.ListNode) *linknode.ListNode {
 	// 解题思路1：双指针，快慢指针，其中慢指针一次移动一步，快指针一次移动两步。
+	// 以上方法只能用于初步判断是否存在环，至于环的入口需要在稿纸上进行一些数学推理，x(起点到入口的距离),y(相遇节点距离x的距离),z(相遇之后的距离)
+	// 可以只记结论：相遇之后，分别以头节点和相遇节点为起点，一次只走一步，二者相遇时，则一定是入口节点。
+
 	// 解题思路2: hashmap，key设为*LinkNode, value为0，1(出现)。
+	var meetNode *linknode.ListNode
 	low, fast := head, head
+
+	// 找到 meetCount
 	for fast != nil {
-		low = low.Next
-		if fast.Next != nil {
-			if fast == fast.Next.Next {
-				return fast
-			}
-			fast = fast.Next.Next
-		} else {
+		if fast.Next == nil {
 			return nil
 		}
+
+		fast = fast.Next.Next
+		low = low.Next
 		if low == fast {
-			return fast.Next
+			meetNode = fast
+			break
 		}
 	}
-	return nil
+	if fast == nil {
+		return nil
+	}
+
+	// 可以只记结论：相遇之后，分别以头节点和相遇节点为起点，一次只走一步，二者相遇时，则一定是入口节点。
+	index1 := head
+	index2 := meetNode
+	for index1 != index2 {
+		index1 = index1.Next
+		index2 = index2.Next
+	}
+	return index1
 }
