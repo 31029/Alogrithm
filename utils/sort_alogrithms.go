afe6c8c39d2ece9fb69a49f4a64cfef795a4c648
h31029 <hzy740876073@gmail.com>
2024-12-10 18:37:55 +0800

    [daily] - 快速排序

--- utils/sort_alogrithms.go
+++ utils/sort_alogrithms.go
@@ -1,11 +1,41 @@
 package utils
 
-func BubbleSort(nums []int) {
-	for i := 0; i < len(nums); i++ {
-		for j := 0; j < len(nums)-i-1; j++ {
-			if nums[j+1] < nums[j] {
-				Swap(nums, j+1, j)
+func BubbleSort(tgt []int) {
+	// 优化过后的bubbleSort
+	for i, change := 0, true; i < len(tgt) && change; i++ {
+		change = false
+		for j := 0; j < len(tgt) - 1 - i; j++ {
+			if tgt[j] > tgt[j+1] {
+				tmp := tgt[j]
+				tgt[j] = tgt[j+1]
+				tgt[j+1] = tmp
+				change = true
 			}
 		}
+	}
+}
+
+func partition(low, high int, nums []int) int {
+	pivot := nums[low]
+	for low < high {
+		for nums[high] > pivot && high > low{
+			high --
+		}
+		nums[low] = nums[high]
+
+		for nums[low] < pivot && high > low{
+			low ++
+		}
+		nums[high] = nums[low]
+	}
+	nums[low] = pivot
+	return low
+}
+
+func QuickSort(low, high int, nums []int)  {
+	if low < high {
+		pivot := partition(low, high, nums)
+		QuickSort(pivot+1, high, nums)
+		QuickSort(0, low, nums)
 	}
 }
--- utils/sort_alogrithms_test.go
+++ utils/sort_alogrithms_test.go
@@ -12,3 +12,12 @@ func TestBubbleSort(t *testing.T) {
 
 	t.Logf("sorted array:%v", t_A)
 }
+
+
+func TestQuickSort(t *testing.T)  {
+	nums := []int{5, 7, 2, 1, 9, 8, 10}
+	QuickSort(0, len(nums)-1, nums)
+	for _, v := range nums {
+		println(v)
+	}
+}
